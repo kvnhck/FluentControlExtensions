45f44ef06bf52d3fc1d1737ad41886564a93a154
Kevin Van Haecke <kvh@dottix.com>
2013-05-24 22:49:33 +0200

    Created some more tests

--- FluentControlExtensions.Test/ControlCollectionTests.cs
+++ FluentControlExtensions.Test/ControlCollectionTests.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.UI;
+﻿using System;
+using System.Linq;
+using System.Web.UI;
 using System.Web.UI.WebControls;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -19,6 +21,22 @@ namespace FluentControlExtensions.Test
             Assert.IsInstanceOfType(txt, typeof(TextBox));
             Assert.AreEqual("txtName", txt.ID);
             CollectionAssert.Contains(page.Controls, txt);
+        }
+
+        [TestMethod]
+        [TestCategory("ControlCollection.ForEach")]
+        public void ChangeAllTextBoxes()
+        {
+            var plh = ControlFactory.Create<PlaceHolder>();
+            foreach (var i in Enumerable.Range(0, 10))
+            {
+                plh.Controls.Create<TextBox>("txt" + i);
+            }
+
+            plh.Controls.ForEach<TextBox>(t => t.CssClass("text"));
+
+            Assert.AreEqual(10, plh.Controls.Count);
+            Assert.AreEqual("text", ((TextBox)(plh.Controls[9])).CssClass);
         }
     }
 }
---
+++ FluentControlExtensions.Test/DataBoundControlTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Web.UI.WebControls;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FluentControlExtensions.Test
+{
+    [TestClass]
+    public class DataBoundControlTests
+    {
+        [TestMethod]
+        [TestCategory("DataBoundControl.DataBind")]
+        public void DataBind()
+        {
+            var ddl = ControlFactory
+                .Create<DropDownList>()
+                .DataBind(new List<string> { "Item 1", "Item 2" });
+
+            Assert.AreEqual(2, ddl.Items.Count);
+            Assert.AreEqual("Item 2", ddl.Items[1].Text);
+        }
+    }
+}
