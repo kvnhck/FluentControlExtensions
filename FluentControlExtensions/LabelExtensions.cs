a60bf2b4bc7a20137356489967ad06dec026866d
Kevin Van Haecke <kvh@dottix.com>
2013-05-24 22:45:40 +0200

    Added LabelExtensions

---
+++ FluentControlExtensions/LabelExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FluentControlExtensions
+{
+    public static class LabelExtensions
+    {
+        public static Label AssociateWith(this Label ctl, string associatedControlId)
+        {
+            ctl.AssociatedControlID = associatedControlId;
+            return ctl;
+        }
+
+        public static Label AssociateWith(this Label ctl, Control associatedControl)
+        {
+            ctl.AssociatedControlID = associatedControl.ID;
+            return ctl;
+        }
+    }
+}
